3b8af79bf824d8b74e47c6530f018023da09afa7
davizapeli-spec <davizapeli@gmail.com>
2025-10-01 17:06:46 -0300

    Add files via upload

---
+++ Calculos/logica.js
@@ -0,0 +1,10 @@
+function soma() {
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+    var resposta = document.getElementById("respSoma")
+   
+    repostatext.numero1+numero2
+    // console.log(numero1, numero2)
+}function sub (){
+    
+}
